c06701b577c45c86303a0e3753237d9e257cb29a
spreadmycode <spreadmycode@outlook.com>
2022-11-30 20:25:05 -0800

    double taping fixed

--- components/AttributeSlot.tsx
+++ components/AttributeSlot.tsx
@@ -1,8 +1,8 @@
-import { LONG_PRESS_DELAY } from "@/libs/constants";
 import { Attribute } from "pages/work";
 import { useEffect, useRef, useState } from "react";
-import { isMobile } from "react-device-detect";
 import { toast } from "react-toastify";
+import { useDoubleTap } from "use-double-tap";
+import useDetectDevice from "@/hooks/useDetectDevice";
 
 type Props = {
   attribute: Attribute;
@@ -14,42 +14,15 @@ type Props = {
 const AttributeSlot = ({ attribute, selectedIds, onClick, onSave }: Props) => {
   const ref = useRef(null);
   const refInput = useRef(null);
-  let start = 0;
+  const { isMobile } = useDetectDevice();
   const [image, setImage] = useState<string>("");
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [isMouseOver, setIsMouseOver] = useState<boolean>(false);
   const [label, setLabel] = useState<string>(attribute.label);
 
-  const onMouseDown = (e: any, id: number) => {
-    if (isMobile) return;
-
-    start = Date.now();
-  };
-
-  const onMouseUp = (e: any, id: number) => {
-    if (isMobile) return;
-
-    onClick(id);
-
-    if (Date.now() - start > LONG_PRESS_DELAY) {
-      setIsEditing(true);
-    }
-  };
-
-  const onTouchStart = (e: any, id: number) => {
-    if (!isMobile) return;
-
-    start = Date.now();
-  };
-
-  const onTouchEnd = (e: any, id: number) => {
-    if (!isMobile) return;
-
-    onClick(id);
-
-    if (Date.now() - start > LONG_PRESS_DELAY) {
-      setIsEditing(true);
-    }
-  };
+  const bind = useDoubleTap((event) => {
+    setIsEditing(true);
+  });
 
   const onFinish = () => {
     if (!label) {
@@ -95,15 +68,15 @@ const AttributeSlot = ({ attribute, selectedIds, onClick, onSave }: Props) => {
   return (
     <div ref={ref} className="w-full flex justify-center items-center">
       <div
-        className={`w-36 h-36 border flex flex-col justify-start items-start overflow-hidden rounded-md select-none ${
+        className={`w-36 h-36 border flex flex-col justify-start items-start overflow-hidden rounded-md select-none relative ${
           selectedIds.includes(attribute.id)
             ? "border-green-500"
             : "border-gray-500"
         } cursor-pointer`}
-        onMouseDown={(e) => onMouseDown(e, attribute.id)}
-        onMouseUp={(e) => onMouseUp(e, attribute.id)}
-        onTouchStart={(e) => onTouchStart(e, attribute.id)}
-        onTouchEnd={(e) => onTouchEnd(e, attribute.id)}
+        onMouseEnter={() => setIsMouseOver(true)}
+        onMouseLeave={() => setIsMouseOver(false)}
+        onMouseDown={() => onClick(attribute.id)}
+        {...bind}
       >
         {image ? (
           <img
@@ -113,6 +86,23 @@ const AttributeSlot = ({ attribute, selectedIds, onClick, onSave }: Props) => {
         ) : (
           <div className="w-full h-32 flex justify-center items-center">
             <div className="animate-spin rounded-full w-10 h-10 border-t-2 border-white"></div>
+          </div>
+        )}
+        {!isEditing && !isMobile && isMouseOver && (
+          <div
+            className="absolute -right-10 -top-10 w-20 h-20 bg-[#00000088] backdrop-blur-sm rounded-full overflow-hidden text-white cursor-pointer hover:bg-green-500 transition-all duration-300"
+            onClick={() => setIsEditing(true)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="currentColor"
+              className="bi bi-pencil-fill absolute bottom-4 left-4"
+              viewBox="0 0 16 16"
+            >
+              <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z" />
+            </svg>
           </div>
         )}
 
--- libs/constants.ts
+++ libs/constants.ts
@@ -1,4 +1,3 @@
 export const TAG_EMAIL = "EMAIL";
 export const TAG_ACCESS_TOKEN = "ACCESS_TOKEN";
 export const TAG_REFRESH_TOKEN = "REFRESH_TOKEN";
-export const LONG_PRESS_DELAY = 200;
