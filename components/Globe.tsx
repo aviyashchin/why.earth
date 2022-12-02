942d86f45ddf1ad616b3e7afb9483b550a6a1e7d
spreadmycode <spreadmycode@outlook.com>
2022-12-01 16:30:15 -0800

    glob js source updated

--- components/Globe.tsx
+++ components/Globe.tsx
@@ -7,10 +7,7 @@ const Globe = () => {
         <div className="Satellite__globe js-globe"></div>
         <span className="array"></span>
       </section>
-      <Script
-        src="https://b.stripecdn.com/site-srv/assets/compiled/js/blog/globe-f22b76d7dcb05af683c3.min.js"
-        strategy="afterInteractive"
-      />
+      <Script src="/js/stripe-globe.js" strategy="afterInteractive" />
     </>
   );
 };
